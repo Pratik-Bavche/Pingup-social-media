0a66312efad3e60913a024299ae3e2863027d049
Pratik-Bavche <pratikbavche2005@gmail.com>
2025-08-06 22:21:48 +0530

    file iported

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { inngest, functions } from "./inngest/index.js"
+import {serve} from 'inngest/express'
 
 const app=express();
 await connectDB();
 
 app.use(express.json());
 app.use(cors());
-//this is server file
 
 app.get('/',(req,res)=>{
     res.send("Server is running")
